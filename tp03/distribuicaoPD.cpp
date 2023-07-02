505841c5c595e1b4c89900a3da67f21b8e2f5773
Aline Cristina Pinto <alinecristina3.14nto@gmail.com>
2023-07-02 19:18:31 -0300

    feat: adds doc

--- tp03/distribuicaoPD.cpp
+++ tp03/distribuicaoPD.cpp
@@ -13,14 +13,12 @@ int DistribuicaoPD::buscarNumeroMinimoLigas(int ligas[], int numTipoLigas, int d
 
     tabela[0] = 0; // nenhuma liga precisa ser utilizada para somar 0
 
-    // para todos os valores entre 1 e a demanda
     for(int i = 1; i <= demanda; i++){
         tabela[i] = INF; // seta a posicao i como infinito ja que ainda nao houve a contabilizacao de nenhuma liga
 
         for (int j = 0; j < numTipoLigas; j++){
-            // verifica se eh possivel usar a liga (tamanho menor que o valor)
             if(ligas[j] <= i){
-                // soma 1 (liga que pode estar sendo adicionada a solucao) a cada solucao armazenada em j
+                // soma 1 (nova liga adicionada a solucao) a cada solucao armazenada em j
                 int subSolucao = tabela[i - ligas[j]] + 1;
 
                 // atualiza o numero de ligas necessarias caso a sub solucao seja menor do que a armazenada na tabela
